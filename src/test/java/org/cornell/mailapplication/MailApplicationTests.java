a7753c6db6887d46bd7c0c607fe7db6ab901dd91
Jingkai Zhang <jz544@cornell.edu>
2022-06-30 16:45:32 -0400

    Initial commit

---
+++ src/test/java/org/cornell/mailapplication/MailApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cornell.mailapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
